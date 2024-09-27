426df76e42edf14b7c4998a7512036c5d61597ce
Swiiish21 <malcolmotieno@gmail.com>
2024-09-27 13:13:41 +0300

    ..

--- simple_arrowFunction_examples.dart
+++ simple_arrowFunction_examples.dart
@@ -8,6 +8,10 @@ var add = (a, b) => a + b;
 print(add(1, 2));
 //output:3
 
+var minus = (c, d) => c - d;
+print(minus(5, 3));
+//output: 2
+
 var multiplication = (x, y) => x * y;
 print(multiplication(2, 3));
 //output: 6
