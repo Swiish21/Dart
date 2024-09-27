63eaf4cef6fde95771acb598eb24f79cd0a5b727
Swiiish21 <malcolmotieno@gmail.com>
2024-09-27 11:55:24 +0300

    added a few lines

---
+++ simple_arrowFunction_examples.dart
@@ -0,0 +1,19 @@
+void main(){
+
+var greet = (name) => 'Hello $name';
+print(greet("Alice"));
+//output: Hello Alice
+
+var add = (a, b) => a + b;
+print(add(1, 2));
+//output:3
+
+var multiplication = (x, y) => x * y;
+print(multiplication(2, 3));
+//output: 6
+
+var sayHi = () => "Hi there!";
+print(sayHi());
+//output: Hi there!
+
+}
