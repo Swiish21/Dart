de5cadbe9cd019eb34ccc695c0452c403f23f0cb
Swiiish21 <malcolmotieno@gmail.com>
2024-10-02 10:56:24 +0300

    major changes

--- classes(oop).dart
+++ classes(oop).dart
@@ -1,27 +1,17 @@
-void main(){
-  class Dog {
-    String name;
-    int age;
-    String breed;
+class Car {
+  String brand;
+  String model;
 
-    void bark(){
-      print("woof");
-    }
-    void eat(){
-      print("nom nom");
-    }
+
+  Car(this.brand, this.model);
+
+  void showDetails(){
+    print('Brand: $brand');
+    print('Model: $model');
   }
+}
 
-  // Create a new instance of the Dog class
-  Dog myDog = Dog();
-  myDog.name = "Fido";
-  myDog.age = 3;
-  myDog.breed = "Golden Retriever";
-
-  // Call the bark and eat methods
-  myDog.bark();
-  myDog.eat();
-
-  // Print out the dog's details
-  print("My dog's name is ${myDog.name}, he is ${myDog.age} years old and is a ${myDog.breed}.");
+void main() {
+  Car myCar = Car('Toyota', 'Corolla');
+  myCar.showDetails();
 }
